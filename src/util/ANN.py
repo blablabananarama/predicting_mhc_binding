7fbcbcd95d63c65b8c662ea926bb9b4fb55768bc
AlexCuber <46822425+AlexCuber@users.noreply.github.com>
2019-06-22 17:31:30 +0200

    new update ANN

--- src/util/ANN.py
+++ src/util/ANN.py
@@ -17,186 +17,18 @@ import keras.backend as K
 from keras.models import load_model
 from scipy.stats import pearsonr
 
-#config = tf.ConfigProto()
-#config.gpu_options.allow_growth = False
-
-#session = tf.Session(config=config)
-#set_session(session)
-
-
-
-#with tf.device('/gpu:0'):
-#  var = tf.Variable(initial_value=([[1,2],[3,4]]))
-#sess = tf.Session()
-#sess.run(var)
-
-config = tf.ConfigProto(
-        device_count = {'GPU': 0}
-    )
-sess = tf.Session(config=config)
-# In[30]:
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 
-#os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
-#os.environ["CUDA_VISIBLE_DEVICES"] = ""
-# In[5]:
-#sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
 
-np.random.seed(7)
+#shuffle = True
+#discrete = True
 
-
-# # Load data
-
-# In[6]:
-
-
-train_data = "../../dataset/A0201/f000"
-valid_data = "../../dataset/A0201/c000"
-
-
-# # Arguments
-
-# In[31]:
-
-
-MAX_PEP_SEQ_LEN=9
-
-print(os.getcwd())
-
-
-# # Functions
-
-# In[23]:
-
-
-
-
-def read_pep(filename, MAX_PEP_SEQ_LEN):
-    '''
-    read AA seq of peptides and MHC molecule from text file
-    parameters:
-        - filename : file in which data is stored
-    returns:
-        - pep_aa : list of amino acid sequences of peptides (as string)
-        - target : list of log transformed IC50 binding values
-    '''
-    pep_aa = []
-    target = []
-    infile = open(filename, "r")
-
-    for l in infile:
-        l = l.strip().split()
-        assert len(l) == 3
-        if len(l[0]) <= MAX_PEP_SEQ_LEN:
-            pep_aa.append(l[0])
-            target.append(l[1])
-    infile.close()
-
-    return pep_aa, target
-
-
-# In[24]:
-
-
-def read_blosum_MN(filename):
-    '''
-    read in BLOSUM matrix
-    parameters:
-        - filename : file containing BLOSUM matrix
-    returns:
-        - blosum : dictionnary AA -> blosum encoding (as list)
-    '''
-
-    # read BLOSUM matrix:
-    blosumfile = open(filename, "r")
-    blosum = {}
-    B_idx = 99
-    Z_idx = 99
-    star_idx = 99
-
-    for l in blosumfile:
-        l = l.strip()
-
-        if l[0] != '#':
-            l = l.strip().split()
-
-            if (l[0] == 'A') and (B_idx==99):
-                B_idx = l.index('B')
-                Z_idx = l.index('Z')
-                star_idx = l.index('*')
-            else:
-                aa = str(l[0])
-                if (aa != 'B') &  (aa != 'Z') & (aa != '*'):
-                    tmp = l[1:len(l)]
-                    # tmp = [float(i) for i in tmp]
-                    # get rid of BJZ*:
-                    tmp2 = []
-                    for i in range(0, len(tmp)):
-                        if (i != B_idx) &  (i != Z_idx) & (i != star_idx):
-                            tmp2.append(float(tmp[i]))
-
-                    #save in BLOSUM matrix
-                    blosum[aa] = tmp2
-    blosumfile.close()
-    return(blosum)
-
-
-# In[51]:
-
-
-def encode_pep(Xin, max_pep_seq_len):
-    '''
-    encode AA seq of peptides using BLOSUM50
-    parameters:
-        - Xin : list of peptide sequences in AA
-    returns:
-        - Xout : encoded peptide seuqneces (batch_size, max_pep_seq_len, n_features)
-    '''
-    # read encoding matrix:
-    blosum = read_blosum_MN("../../dataset/BLOSUM50")
-    n_features = len(blosum['A'])
-    n_seqs = len(Xin)
-
-    # make variable to store output:
-    Xout = np.zeros((n_seqs, max_pep_seq_len, n_features),
-                       dtype=np.uint8)
-
-    for i in range(0, len(Xin)):
-        for j in range(0, len(Xin[i])):
-            Xout[i, j, :n_features] = blosum[ Xin[i][j] ]
-    return Xout
-
-
-# # Main
-
-# ## Read data
-
-# In[40]:
-
-
-# read in peptide sequences and targets:
-X_train_raw, y_train_raw = read_pep(train_data, MAX_PEP_SEQ_LEN)
-X_val_raw, y_val_raw = read_pep(valid_data, MAX_PEP_SEQ_LEN)
-
-
-# ## Encode data
-
-# In[41]:
-
-
-
-shuffle = True
-discrete = True
-# encode data using BLOSUM50:
-X_train = encode_pep(X_train_raw, MAX_PEP_SEQ_LEN)
-y_train = np.array(y_train_raw)
-X_val = encode_pep(X_val_raw, MAX_PEP_SEQ_LEN)
-y_val = np.array(y_val_raw)
 
 def ANN(X_train, y_train, X_val, y_val, model_name):
+    
+    EPOCHS = 100
+    MINI_BATCH_SIZE = 120
     n_features = X_train.shape[2]
     
     X_train = X_train.reshape(X_train.shape[0], -1)
@@ -246,21 +78,9 @@ def ANN(X_train, y_train, X_val, y_val, model_name):
     
     # data dimensions now:
     # (N_SEQS, SEQ_LENGTH, N_FEATURES)
-    print(X_train.shape)
-    print(X_val.shape)
+#    print(X_train.shape)
+ #   print(X_val.shape)
 
-
-
-# In[43]:
-
-
-# Reshape data
-    
-    # ## Compile model
-    
-    # In[44]:
-    
-    
     # create model
     model = Sequential()
     keras.layers.normalization.BatchNormalization(epsilon=1e-06, momentum=0.95, weights=None)
@@ -277,22 +97,8 @@ def ANN(X_train, y_train, X_val, y_val, model_name):
     
     
     model.summary()
-    
-    
-    # ## Fit model
-    
-    # In[46]:
-    
-    
-    EPOCHS = 100
-    MINI_BATCH_SIZE = 120
-    
-    
-    # In[47]:
-    
-    print(X_train.shape)
-    #%%
-    
+
+#    print(X_train.shape)
     
     # Fit the model
     history = model.fit(X_train, y_train, epochs=EPOCHS, batch_size=MINI_BATCH_SIZE, validation_data=(X_val, y_val))
@@ -308,4 +114,4 @@ def ANN(X_train, y_train, X_val, y_val, model_name):
         y_pred =(y_pred > 0.5)    
     return train_loss, test_loss, y_val, y_pred, model_path #y_real, y_predict, history
 
-a,b,c,d,e = ANN(X_train, y_train, X_val, y_val, "ANN")
+#a,b,c,d,e = ANN(X_train, y_train, X_val, y_val, "ANN")
